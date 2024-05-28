9591d9524bb0eee8e3eec56384e34473d0d14ae4
Z0mbie001 <94248812+Z0mbie001@users.noreply.github.com>
2024-05-28 15:04:40 +0100

    Remove Supabase API Key from main

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // Install the supabse-js module
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from '';
 
 // Create a single supabase client
-export const supabase = createClient('https://zndpnqsommwahmmdkmmc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuZHBucXNvbW13YWhtbWRrbW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyOTA0MDMsImV4cCI6MjAzMTg2NjQwM30.SOviPkL0lHuGRIP0OLMFCPXq9KhixmPEB4s6Z5Arcjk');
+export const supabase = createClient('', '');
